478d5acd7093324f53287bd8e293e07e6419f072
Bohinich Serhii <bohinich1@gmail.com>
2025-06-22 14:25:45 +0300

    .

--- src/components/Searchbar/Searchbar.tsx
+++ src/components/Searchbar/Searchbar.tsx
@@ -1,4 +1,5 @@
-import React, { useState, ChangeEvent, FormEvent } from 'react';
+import React, { useState } from 'react';
+import type { ChangeEvent, FormEvent } from 'react';
 
 interface SearchbarProps {
   onSubmit: (query: string) => void;
